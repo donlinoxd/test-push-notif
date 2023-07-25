9d9850b53a0031c7f556d7373e55679d2f05b597
don.lino.xd <don.lino.xd@gmail.com>
2023-07-25 18:28:44 +0800

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,30 @@ import { messaging } from '../src/lib/firebase'
 export default function Home() {
     function requestPermission() {
         console.log('Requesting permission...')
-        window?.Notification.requestPermission().then((permission) => {
+        Notification.requestPermission().then((permission) => {
             if (permission === 'granted') {
                 console.log('Notification permission granted.')
             }
         })
+
+        const messaging = getMessaging()
+        getToken(messaging, { vapidKey: 'BKIVHHmhVEKzCmMy9ukLTjhfuAlluEyJabTl4RTVFTm4FZOMMGbJBX6TE26LgMEcsZisU4W2LDvNpK42hliOsM8' })
+            .then((currentToken) => {
+                if (currentToken) {
+                    // Send the token to your server and update the UI if necessary
+                    // ...
+                    console.log('Send the token to the server')
+                    console.log('current Token: ', currentToken)
+                } else {
+                    // Show permission request UI
+                    console.log('No registration token available. Request permission to generate one.')
+                    // ...
+                }
+            })
+            .catch((err) => {
+                console.log('An error occurred while retrieving token. ', err)
+                // ...
+            })
     }
 
     useEffect(() => {
